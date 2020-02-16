be11e5b3a48d95fa7e7bcd39c2012cf2f8846ef2
KevinJ0 <kevinjooo59@gmail.com>
2020-02-15 20:26:16 -0400

    primer commit

---
+++ SchoolSystem/Data/SchoolSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Models;
+
+namespace SchoolSystem.Data
+{
+    public class SchoolSystemContext : DbContext
+    {
+        public SchoolSystemContext (DbContextOptions<SchoolSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SchoolSystem.Models.Student> Student { get; set; }
+    }
+}
---
+++ SchoolSystem/Migrations/20200215164610_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolSystem.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true),
+                    lastName = table.Column<string>(nullable: true),
+                    status = table.Column<bool>(nullable: false),
+                    career = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
